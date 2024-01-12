54d1c485bfd79994331e489962982aedd820c4f8
gfigueras <guillermofiguerasjimenez@MacBook-Air-de-Guillermo.local>
2024-01-12 19:35:15 +0100

    adding portfolio download

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -1,5 +1,7 @@
 import '../styles/secondPage.css'
 import profileImage from '../assets/profileImage3.webp'
+import cvPDF from '../assets/CV-GFIGUERAS.pdf';
+
 const desc = "Entusiasta de la tecnología que desde su infancia ha desarrollado una pasión por el hardware y el ensamblaje de ordenadores. Como técnico en sistemas microinformáticos y redes, encuentra satisfacción resolviendo desafíos tecnológicos. <br /><br />Actualmente, se encuentra en su segundo año de estudios en desarrollo de aplicaciones multiplataforma y cuenta con experiencia en Java/Kotlin, SQL y CSS. <br /><br />Su dedicación y conocimientos en el mundo de la tecnología son la base para aportar soluciones innovadoras a cualquier proyecto."
 export const SecondPage = () => {
     return (
@@ -10,7 +12,9 @@ export const SecondPage = () => {
                 <div className="left-side-about">
                     <h1>ABOUT ME</h1>
                     <p dangerouslySetInnerHTML={{ __html: desc }}></p>
-                    <button className='download-cv'>DOWNLOAD CV</button>
+                    <button  className='download-cv'>
+                    <a className='download-button' href={cvPDF} download >DOWNLOAD CV</a>
+                        </button>
                     
 
                 </div>
