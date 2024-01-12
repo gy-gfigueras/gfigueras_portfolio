cdfe39cd7ae614a624921161aa3ea004f06a95c5
gfigueras <guillermofiguerasjimenez@MacBook-Air-de-Guillermo.local>
2024-01-12 17:09:16 +0100

    finishing page and modifying some things

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import '../styles/mainPage.css'
 import { Navbar } from './Navbar'
 import { Parallax } from './Parallax'
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,12 @@
 import { NavbarListComponent } from './NavbarListComponent';
-import headerLogo from '../assets/gfigueras.png';
+import headerLogo from '../assets/gfigueras.webp';
 import '../styles/navbar.css';
 import { useState, useEffect } from 'react';
 
 export function Navbar() {
-  const [anchoPantalla, setAnchoPantalla] = useState(window.innerWidth);
-
-  const actualizarDimensiones = () => {
-    setAnchoPantalla(window.innerWidth);
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', actualizarDimensiones);
-
-    return () => {
-      window.removeEventListener('resize', actualizarDimensiones);
-    };
-  }, []);
-
-  const esMovil = anchoPantalla <= 768; // Puedes ajustar este umbral según tus necesidades
-
-  const handleClick = () => {
-    if (esMovil) {
-        let header = document.querySelector('.header')
-        let image = document.querySelector('.header-logo')
-      header.style.height = '100vh';
-      header.style.display = "flex";
-      image.style.height = "30px";
-      image.style.alignSelf = "start"
-      image.style.marginTop = "35%"
-      
-    }
-    // Puedes agregar más lógica si es necesario
-  };
-
   return (
-    <header className={`header ${esMovil ? 'mobile' : ''}`}>
-      <a href="#main" className="image-link" onClick={handleClick}>
+    <header className="header">
+      <a href="#main" className="image-link">
         <img className="header-logo" src={headerLogo} alt="" />
       </a>
       <ul className="ul-navbar">
